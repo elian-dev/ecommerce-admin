883a7498eb6d2dc88d550bcfdbd872463e42dcdf
elian-dev <geordymemdoza@gmail.com>
2023-09-16 14:58:15 -0600

    feat: :sparkles: Add orders frontend

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { ArrowUpDown } from "lucide-react"
+import { ColumnDef } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import { CellAction } from "./cell-action"
+
+export type OrderColumn = {
+  id: string
+  phone: string
+  address: string
+  isPaid: boolean
+  totalPrice: string
+  products: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "products",
+    header: "Products"
+  },
+  {
+    accessorKey: "phone",
+    header: "Phone"
+  },
+  {
+    accessorKey: "address",
+    header: "Address"
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Total price"
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Paid"
+  },
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Date
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+]
